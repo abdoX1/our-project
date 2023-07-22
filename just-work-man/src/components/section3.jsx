cef348f2681afc3d249c8d16669b765f01fe836b
abdoX1 <125511739+abdoX1@users.noreply.github.com>
2023-07-22 23:52:25 +0100

    finished the footer

--- just-work-man/src/components/section3.jsx
+++ just-work-man/src/components/section3.jsx
@@ -3,7 +3,7 @@ import SearchBar from "./SearchBar";
 
 function Section3() {
     return (
-        <div style={{backgroundColor: '#00112C'}} className="text-center py-28 text-white">
+        <div style={{backgroundColor: '#04294F'}} className="text-center py-28 text-white">
             <h2 className="text-5xl font-extrabold py-5">Build your online business</h2>
             <p className="tracking-wide py-5 pb-8">Lay a solid foundation for your business with a killer domain name.</p>
             <div className="">
